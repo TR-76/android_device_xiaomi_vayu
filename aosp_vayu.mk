272ede019d14b032a320b8a6badb7ddd72f3fe60
kleidione Freitas <kleidione@gmail.com>
2021-03-25 22:49:49 -0300

    vayu: Start changes to kraken
    Signed-off-by: kleidione <kleidione@gmail.com>

--- aosp_vayu.mk
+++ aosp_vayu.mk
@@ -8,13 +8,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Build type
+KRAKEN_BUILD_TYPE := OFFICIAL
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit common configurations
+$(call inherit-product, vendor/aosp/config/common.mk)
 
-PRODUCT_NAME := arrow_vayu
+PRODUCT_NAME := aosp_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
@@ -23,11 +29,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys" \
-    PRODUCT_NAME=vayu_global \
-    PRODUCT_MODEL=M2102J20SI
+    PRIVATE_BUILD_DESC="vayu_global-user 11 RKQ1.200826.002 V12.5.5.0.RJUMIXM release-keys"
 
-BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
-
-DEVICE_MAINTAINER := kubersharma001
+BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:11/RKQ1.200826.002/V12.5.5.0.RJUMIXM:user/release-keys
 
