244fe5151747f98ef72c9ca51e86d27f28ee32c4
TR-76 <tr-76@gmx.at>
2021-08-16 16:22:59 +0200

    Remove Packages
    Signed-off-by: TR-76 <tr-76@gmx.at>

--- device.mk
+++ device.mk
@@ -491,5 +491,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
+    
+# Remove Packages
+PRODUCT_PACKAGES += \
+    RemovePackages
 
 include vendor/xiaomi/vayu/vayu-vendor.mk
