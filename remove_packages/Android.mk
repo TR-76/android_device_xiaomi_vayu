1541200c8049673071c612676e63f281a75d7dd2
TR-76 <tr-76@gmx.at>
2021-11-18 07:25:10 +0100

    Remove Packages
    Signed-off-by: TR-76 <tr-76@gmx.at>

---
+++ remove_packages/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := SimpleCalendar Calendar SimpleGallery Gallery2 DuckDuckGo Browser2 GCamGo Camera2 Email UnifiedEmail messaging
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
