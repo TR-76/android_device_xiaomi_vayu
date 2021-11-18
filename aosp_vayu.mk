34e579f1a3b3de8aa04f157c11c2820bbe7c3a51
TR-76 <tr-76@gmx.at>
2021-11-18 08:31:45 +0100

    PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
    Signed-off-by: TR-76 <tr-76@gmx.at>

--- aosp_vayu.mk
+++ aosp_vayu.mk
@@ -20,6 +20,7 @@ $(call inherit-product, vendor/aosp/config/common.mk)
 
 # MG-APKS
 $(call inherit-product-if-exists, vendor/mg-apks/config.mk)
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
 
 PRODUCT_NAME := aosp_vayu
 PRODUCT_DEVICE := vayu
