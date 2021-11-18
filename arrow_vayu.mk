7dedb679ffa20bfbfff36dd95c5e0bcc1c90087b
TR-76 <tr-76@gmx.at>
2021-11-18 08:31:45 +0100

    PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
    Signed-off-by: TR-76 <tr-76@gmx.at>

--- arrow_vayu.mk
+++ arrow_vayu.mk
@@ -16,6 +16,7 @@ $(call inherit-product, vendor/arrow/config/common.mk)
 
 # MG-APKS
 $(call inherit-product-if-exists, vendor/mg-apks/config.mk)
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
 
 TARGET_SUPPORTS_QUICK_TAP := true
 
