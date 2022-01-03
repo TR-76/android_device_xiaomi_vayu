0e884f054ff55f80cd0eb37bb12f5bfff9cc1b3b
TR-76 <tr-76@gmx.at>
2022-01-03 11:32:40 +0100

    Remove GCamGOPrebuilt
    Signed-off-by: TR-76 <tr-76@gmx.at>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := SimpleCalendar Calendar SimpleGallery Gallery2 DuckDuckGo Browser2 GCamGo Camera2 Email UnifiedEmail messaging
+LOCAL_OVERRIDES_PACKAGES := SimpleCalendar Calendar SimpleGallery Gallery2 DuckDuckGo Browser2 GCamGo GCamGOPrebuilt GCamGOPrebuilt-V2 Camera2 Email UnifiedEmail messaging
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
