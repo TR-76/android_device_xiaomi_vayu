55ed38ae42a0d880166e6d5cadc77847062f0472
TR-76 <tr-76@gmx.at>
2022-02-02 10:49:36 +0100

    TARGET_KERNEL_CLANG_VERSION
    Signed-off-by: TR-76 <tr-76@gmx.at>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,7 +94,7 @@ TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/vayu
 TARGET_KERNEL_ADDITIONAL_FLAGS := LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
-TARGET_KERNEL_CLANG_VERSION := vortex
+TARGET_KERNEL_CLANG_VERSION := mcd
 
 # Partitions
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
