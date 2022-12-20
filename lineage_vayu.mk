6f55907519b0713a742ab82ab3bd9ad9dc11d2c3
TR-76 <tr-76@gmx.at>
2022-12-20 17:18:44 +0100

    Add microG
    Signed-off-by: TR-76 <tr-76@gmx.at>

--- lineage_vayu.mk
+++ lineage_vayu.mk
@@ -14,6 +14,10 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
+# microG
+$(call inherit-product-if-exists, vendor/microg/config.mk)
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
+
 PRODUCT_NAME := lineage_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_MANUFACTURER := Xiaomi
