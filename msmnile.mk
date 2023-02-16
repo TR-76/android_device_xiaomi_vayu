9bf0adde1d86127dd368cc27acb6138e28bba3a5
TR-76 <tr-76@gmx.at>
2023-02-16 16:11:46 +0100

    Remove FM Radio
    Signed-off-by: TR-76 <tr-76@gmx.at>

--- msmnile.mk
+++ msmnile.mk
@@ -221,12 +221,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.fingerprintextension@1.0.vendor
 
-# FM
-PRODUCT_PACKAGES += \
-    FM2 \
-    libqcomfm_jni \
-    qcom.fmradio
-
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
