b1f4dbbfb4ab61b632cd56ddf10b085555d96e9e
TR-76 <tr-76@gmx.at>
2023-03-23 17:30:35 +0100

    Add Mulch Webview
    Signed-off-by: TR-76 <tr-76@gmx.at>

--- lineage_vayu.mk
+++ lineage_vayu.mk
@@ -18,6 +18,9 @@ $(call inherit-product, device/xiaomi/vayu/device.mk)
 $(call inherit-product-if-exists, vendor/microg/config.mk)
 PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
 
+# Mulch Webview
+$(call inherit-product-if-exists, vendor/mulch/mulch.mk)
+
 PRODUCT_NAME := lineage_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_MANUFACTURER := Xiaomi
