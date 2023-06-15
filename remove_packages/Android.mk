3532b424bb09cc9c8b34a2f4186113cc98578eee
TR-76 <tr-76@gmx.at>
2023-06-15 19:02:23 +0200

    Remove more packages
    Signed-off-by: TR-76 <tr-76@gmx.at>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Eleven Etar ExactCalculator Gallery2 Jelly messaging
+LOCAL_OVERRIDES_PACKAGES := Clock Contacts Eleven Etar ExactCalculator DeskClock Dialer Gallery2 Jelly messaging
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
