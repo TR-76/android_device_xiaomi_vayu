f2029802e3a5bea1c790e9f64558d30f92176508
TR-76 <tr-76@gmx.at>
2023-12-12 17:41:41 +0100

    Remove Packages
    Signed-off-by: TR-76 <tr-76@gmx.at>

--- device.mk
+++ device.mk
@@ -48,6 +48,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     PixelSetupWizardResTarget
 
+# Remove Packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
