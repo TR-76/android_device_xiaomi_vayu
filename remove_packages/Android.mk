ec358decc7836dc826469fb8f92c06499229a217
TR-76 <tr-76@gmx.at>
2024-02-19 20:11:18 +0100

    Remove Glimpse
    Signed-off-by: TR-76 <tr-76@gmx.at>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Aperture Clock Contacts Eleven Etar ExactCalculator DeskClock Dialer Gallery2 Jelly messaging
+LOCAL_OVERRIDES_PACKAGES := Aperture Clock Contacts Eleven Etar ExactCalculator DeskClock Dialer Gallery2 Glimpse Jelly messaging
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
