1f917d677fdc7ac890f23147be1f9c1d7fe85749
TR-76 <tr-76@gmx.at>
2024-04-22 19:32:00 +0200

    TARGET_EXCLUDES_AUDIOFX := true
    Signed-off-by: TR-76 <tr-76@gmx.at>

--- lineage_vayu.mk
+++ lineage_vayu.mk
@@ -10,6 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_EXCLUDES_AUDIOFX := true
 
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
