ad9e4a1b08c36a354c95ae50a7d947d03b6fb21c
TR-76 <tr-76@gmx.net>
2026-08-25 20:47:18 +0200

    Revert "build Cromite Webview if exists"
    This reverts commit 2ad6cc3a07dd8a62fdcbd4ad1cc4a9023c90a75f.

--- lineage_vayu.mk
+++ lineage_vayu.mk
@@ -15,9 +15,6 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Cromite Webview
-$(call inherit-product-if-exists, vendor/cromite/cromite.mk)
-
 # microG
 $(call inherit-product-if-exists, vendor/microg/config.mk)
 PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
